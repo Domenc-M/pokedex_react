c80116e10524f771e3bfab5af1c30e6d2b097e31
Maxime <malryck33@gmail.com>
2023-04-12 19:50:15 +0200

    get pokemon stats from api

--- src/pages/PokeDetails.js
+++ src/pages/PokeDetails.js
@@ -2,16 +2,32 @@ import Pokepic from '../components/Pokepic.js';
 import {
     useParams,
   } from "react-router-dom";
+import axios from 'axios';
+
+
 
 const Details = () => {
 
     let params = useParams();
     console.log(params.id);
+    let pokemon;
 
+    
+    axios.get("https://pokeapi.co/api/v2/pokemon/"+ params.id +"")
+    .then(
+      (result) => {
+        pokemon = result.data;
+        console.log(pokemon);
+        });
+
+
+    
+    
     return (
         <div id="home">
             <main>
             <Pokepic pid={params.id}/>
+            {pokemon.height}
             </main>
         </div>
     );

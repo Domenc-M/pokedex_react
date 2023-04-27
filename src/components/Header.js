04644efb49253d677eda8a4e09cefc085011985a
Maxime <malryck33@gmail.com>
2023-04-27 19:08:28 +0200

    remove searchbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,10 @@
 import '../scss/header.scss'
 import axios from 'axios';
 
-function autocompleteSearch() {
-    let input = document.getElementById("searchbar");
-
-    console.log(input.value);
-    axios.get("https://pokeapi.co/api/v2/pokemon/" + input.value + "")
-    .then(
-        (data) => {
-            console.log(data)
-        }
-        );
-  }
-
 const HeaderLayout =() => {
     return(
         <header>
             <h1>Pokédex</h1>
-            <input id="searchbar" type="text" onChange={autocompleteSearch} placeholder="Search..."/>
         </header>
     )
 }
